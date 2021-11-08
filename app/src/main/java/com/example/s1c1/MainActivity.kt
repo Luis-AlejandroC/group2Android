2804d7da83b7ad0b7e39719be34c533bb8de623a
milena beltran <milenab2605@gmail.com>
2021-11-08 11:16:40 -0500

    primer mensaje

---
+++ app/src/main/java/com/example/s1c1/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.s1c1
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.s1c1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
