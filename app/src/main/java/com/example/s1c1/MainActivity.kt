67bfc411160a86607dfd7985639b7c5607d6878a
Hernan D. Belalcazar A <hernandba@gmail.com>
2021-11-09 20:18:37 -0500

    Intent para RegisterActivity en MainActivity - funcion onRegister para enviar a ActivityRegister

--- app/src/main/java/com/example/s1c1/MainActivity.kt
+++ app/src/main/java/com/example/s1c1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.s1c1
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -13,11 +14,13 @@ import com.example.s1c1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-
+    fun onRegister(view: android.view.View) {
+        val intentRegister = Intent(this, RegisterActivity::class.java)
+        startActivity(intentRegister)
     }
 }
