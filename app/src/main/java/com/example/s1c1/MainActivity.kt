5b6f76cd9e7784cacef6bc4d622ee1f530ea6ae4
milena beltran <milenab2605@gmail.com>
2021-11-22 07:50:06 -0500

    sprint 2

--- app/src/main/java/com/example/s1c1/MainActivity.kt
+++ app/src/main/java/com/example/s1c1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.s1c1
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -9,15 +10,37 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
 import com.example.s1c1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
+    private var edtUserName: EditText? = null
+    private var edtPassword: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        edtUserName = findViewById(R.id.edtUsername)
+        edtPassword = findViewById(R.id.edtPassword)
 
     }
+
+    fun onLogin(botonLogin: View)
+    {
+        if (edtUserName!!.text.toString() == "milena@email.com")
+        {
+            if ( edtPassword!!.text.toString()== "1234")
+            {
+                val intento = Intent(this, WelcomeActivity::class.java)
+                startActivity(intento)
+            }
+
+        }
+    }
+    fun onsingUp(botonsingUp: View) {}
 }
+
+
+
